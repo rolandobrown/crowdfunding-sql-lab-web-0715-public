4f48c69e4c2c325362c47a6927d2a108713b1dd6
rolandobrown <rolando.brown@gmail.com>
2015-08-17 11:06:56 -0400

    all done

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,21 @@
--- your code here
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal DECIMAL,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age NUMBER
+);
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER,
+  project_id INTEGER,
+  amount INTEGER
+);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 # Write your sql queries in this file in the appropriate method like the example below:
-# 
+#
 # def select_category_from_projects
 # "SELECT category FROM projects;"
 # end
@@ -7,25 +7,43 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts
-"Write your SQL query Here"
+  "SELECT title, SUM(amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges
-"Write your SQL query Here"
+  "SELECT name, age, SUM(amount) FROM users JOIN pledges ON pledges.user_id = users.id GROUP BY name;"
 end
 
 def selects_the_titles_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+  "SELECT title, (funding_goal - SUM(amount)) AS goal_left
+  FROM projects
+  JOIN pledges
+  ON projects.id = pledges.project_id
+  GROUP BY title
+  HAVING goal_left <= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
-"Write your SQL query Here"
+  "SELECT name, SUM(amount) AS total
+  FROM users
+  JOIN pledges
+  ON users.id = pledges.user_id
+  GROUP BY name
+  ORDER BY total"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+  "SELECT category, amount
+  FROM projects
+  JOIN pledges
+  ON projects.id = pledges.project_id
+  WHERE category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
-"Write your SQL query Here"
+  "SELECT category, SUM(amount)
+  FROM projects
+  JOIN pledges
+  ON projects.id = pledges.project_id
+  WHERE category = 'books'"
 end
